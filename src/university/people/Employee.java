4d8a94b92194ff0a495144437996432c4c3a322f
Miguel Garcia <miguel.garcia.garcia1201@gmail.com>
2023-02-26 16:03:14 -0600

    correccion employee

--- src/university/people/Employee.java
+++ src/university/people/Employee.java
@@ -1,5 +1,13 @@
 package university.people;
 
 public class Employee extends Person {
+    private String rpe;
+
+    public void setRpe(String rpe) {
+        this.rpe = rpe;
+    }
+    public String getRpe() {
+        return rpe;
+    }
 
 }
